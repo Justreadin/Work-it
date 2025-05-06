743b30fea392f9f008a5df90dcb3e61eef67efd4
Favour Akintoye <168464842+Justreadin@users.noreply.github.com>
2025-05-06 08:50:12 +0000

    i added redis url

--- Backend/config/redis.js
+++ Backend/config/redis.js
@@ -1,13 +1,11 @@
-// config/redis.js
 const Redis = require('ioredis');
+const url = require('url');
 
-// Create and configure the Redis client
-const redis = new Redis({
-  host: process.env.REDIS_HOST || 'localhost', // Redis server
-  port: process.env.REDIS_PORT || 6379,       // Redis default port
-  password: process.env.REDIS_PASSWORD || '', // Optional if Redis is password protected
-  db: process.env.REDIS_DB || 0              // Default Redis database
-});
+// Parse the REDIS_URL from environment variables
+const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379'; // Default to localhost if no REDIS_URL is provided
+
+// Use the parsed URL to configure the Redis client
+const redis = new Redis(redisUrl);
 
 redis.on('connect', () => {
   console.log('Connected to Redis');
